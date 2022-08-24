48d6d3a563c6a48e349f107a53d0ef313a0750f2
fernando-aristizabal <fernandoa@disroot.org>
2022-08-24 15:56:51 +0000

    Loading rasters

--- gval/utils/loading_datasets.py
+++ gval/utils/loading_datasets.py
@@ -3,15 +3,26 @@ import rasterio
 
 def load_single_rasterio_file(source):
     """
-    Loads a single raster
+    Utility function loads a single raster from file path or URL checking if not already a raster.
+    
+    Parameters
+    ----------
+
+    Returns
+    -------
+
     """
+    
     # rasterio dataset
     if isinstance(source,rasterio.io.DatasetReader):
         output = source
+    
     # local file path or S3 url
     if isinstance(source,str):
         # TO-DO: support authentication
         output = rasterio.open(source)
+    
+    # if neither rasterio dataset, filepath, or url
     else:
         raise ValueError("Source should be Rasterio DatasetReader or either a file path or a URL to a raster file.")
 
